2645e39d1627981088831c1cbbf82a9ff9aadbfb
juanb <mail2juanb@yahoo.fr>
2024-11-18 18:21:05 +0100

    INIT - first commit

---
+++ src/main/java/com/oc_P6/PayMyBuddy_v1/PayMyBuddyV1Application.java
@@ -0,0 +1,13 @@
+package com.oc_P6.PayMyBuddy_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc_P6/PayMyBuddy_v1/PayMyBuddyV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc_P6.PayMyBuddy_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
