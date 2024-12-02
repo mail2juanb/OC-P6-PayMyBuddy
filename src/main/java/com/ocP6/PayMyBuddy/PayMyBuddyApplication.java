2c214e2ed4eaa38986dc3878a270bd10959969a5
juanb <mail2juanb@yahoo.fr>
2024-12-02 23:29:43 +0100

    style: cleanup

--- src/main/java/com/ocP6/PayMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/ocP6/PayMyBuddy/PayMyBuddyApplication.java
@@ -1,28 +1,13 @@
 package com.ocP6.PayMyBuddy;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@Slf4j
 @SpringBootApplication
 public class PayMyBuddyApplication {
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(PayMyBuddyApplication.class, args);
-
+        SpringApplication.run(PayMyBuddyApplication.class, args);
     }
-
 }
