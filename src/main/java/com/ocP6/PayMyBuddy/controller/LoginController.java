dfe9db026b40ea84b518f7c775036a89d1eb9518
juanb <mail2juanb@yahoo.fr>
2024-12-03 15:16:18 +0100

    feat: Login workflow (controller, view)

---
+++ src/main/java/com/ocP6/PayMyBuddy/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.ocP6.PayMyBuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String loginRoot() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginRequest(@RequestParam String email,
+                               @RequestParam String password,
+                               Model model) {
+        // Validation du formulaire ?
+
+        return "redirect:/login?success";
+    }
+}
