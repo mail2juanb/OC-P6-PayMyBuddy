a29d7e35635ff3a02a4118a29d291e4961efa0a4
juanb <mail2juanb@yahoo.fr>
2024-12-16 14:06:48 +0100

    test: createCustomer non-passing case

--- src/test/java/com/ocP6/PayMyBuddy/service/CustomerServiceImplTest.java
+++ src/test/java/com/ocP6/PayMyBuddy/service/CustomerServiceImplTest.java
@@ -1,61 +1,50 @@
 package com.ocP6.PayMyBuddy.service;
 
+import com.ocP6.PayMyBuddy.exception.ConflictException;
+import com.ocP6.PayMyBuddy.model.Customer;
 import com.ocP6.PayMyBuddy.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.math.BigDecimal;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
 
-    @Autowired
+    @InjectMocks
     private CustomerServiceImpl customerService;
 
-    @Autowired
+    @Mock
     private CustomerRepository customerRepository;
 
-    @Autowired
+    @Mock
     private PasswordEncoder passwordEncoder;
 
 
+
+
     @Test
-    void createCustomerTest() throws Exception{
+    void createCustomer_shouldThrowConflictException_whenEmailAlreadyExists() throws Exception{
 
-        //GIVEN a new customer
-        final String username = "John";
-        final String email = "john.snow@test.test";
-        final String rawPassword = "azerty123";
+        // Given a customer already registred
+        final String username = "user";
+        final String email = "user@user.com";
+        final String rawPassword = "user";
 
+        when(customerRepository.findByEmailIgnoreCase(email)).thenReturn(Optional.of(new Customer()));
 
-        // WHEN the user try to be registred
-        customerService.createCustomer(username, email, rawPassword);
-
-
-        // THEN
-        BigDecimal zero = BigDecimal.ZERO.setScale(2);
-
-        assertThat(customerRepository.findByUsername(username))
-                .isPresent()
-                .get()
-                .satisfies(user -> {
-                    assertThat(user.getEmail()).isEqualTo(email);
-                    assertThat(user.getBalance()).isEqualTo(zero);
-                    assertTrue(passwordEncoder.matches(rawPassword, user.getPassword()));
-                } );
-
-
-
-
+        // When the user try to be registred // Then a Conflict Exception is thrown
+        assertThatThrownBy(() -> customerService.createCustomer(username, email, rawPassword))
+                .isInstanceOf(ConflictException.class)
+                .hasMessageContaining("already exist");
+        verify(customerRepository, never()).save(any(Customer.class));
 
     }
 
